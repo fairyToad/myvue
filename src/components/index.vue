09b463e82ad79c4d15fb9be0e137ffb2f6252837
fairyToad <toadletter@gmail.com>
2020-04-20 15:42:53 +0800

    项目名改为自来也的美多商城

--- src/components/index.vue
+++ src/components/index.vue
@@ -4,7 +4,7 @@
      
    <section class="header text-center">
 		<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
-			<div class="container"><a class="navbar-brand" href="index.html"><i class="fas fa-shopping-bag primary-color mr-1"></i>美多商城</a>
+			<div class="container"><a class="navbar-brand" href="index.html"><i class="fas fa-shopping-bag primary-color mr-1"></i>自来也的美多商城</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-1" aria-controls="navbar-1" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 				<div class="collapse navbar-collapse pull-xs-right justify-content-end" id="navbar-1">
 					<ul class="navbar-nav mt-2 mt-md-0">
